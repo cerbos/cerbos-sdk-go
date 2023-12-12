4ec6f1d113c538ead65ac0d2044366b043b8f286
Charith Ellawala <charith@cerbos.dev>
2023-12-12 15:51:20 +0000

    enhancement: Ability to add custom headers to requests
    Allow setting gRPC header metadata for requests.
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- internal/model.go
+++ internal/model.go
@@ -3,9 +3,24 @@
 
 package internal
 
-import requestv1 "github.com/cerbos/cerbos/api/genpb/cerbos/request/v1"
+import (
+	"context"
+
+	"google.golang.org/grpc/metadata"
+
+	requestv1 "github.com/cerbos/cerbos/api/genpb/cerbos/request/v1"
+)
 
 type ReqOpt struct {
 	AuxData     *requestv1.AuxData
+	Metadata    metadata.MD
 	IncludeMeta bool
+}
+
+func (o *ReqOpt) Context(ctx context.Context) context.Context {
+	if o == nil || len(o.Metadata) == 0 {
+		return ctx
+	}
+
+	return metadata.NewOutgoingContext(ctx, o.Metadata)
 }
